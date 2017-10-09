2b761273c776c049c002f7037b67d84668c786c2
singhcpt <singhcpt@gmail.com>
2017-10-08 23:33:12 -0400

    adjusted comment

--- Aware/app/src/main/java/com/example/shubhankar/aware/MainActivity.java
+++ Aware/app/src/main/java/com/example/shubhankar/aware/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //DOH ODH DOH
+        //doh doh doh
     }
 }
