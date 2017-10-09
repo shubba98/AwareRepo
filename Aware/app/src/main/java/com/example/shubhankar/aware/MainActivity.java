76f0e109557b86b0de503c77f180284246a36a3a
Shubhankar Sachdev <shubhankar@wireless-10-105-121-78.umd.edu>
2017-10-09 00:21:37 -0400

    TESTING

--- Aware/app/src/main/java/com/example/shubhankar/aware/MainActivity.java
+++ Aware/app/src/main/java/com/example/shubhankar/aware/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //DOH2
         //DOH ODH DOH
         //doh doh doh
     }
